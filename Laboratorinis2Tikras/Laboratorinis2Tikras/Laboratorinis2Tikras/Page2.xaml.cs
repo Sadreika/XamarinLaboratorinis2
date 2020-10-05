0258bdf7343c4d10071d56efd083c6ca8fd38c90
MrMariu5 <marius.sadreika@gmail.com>
2020-10-05 20:57:02 +0300

    Added laboratorinis

---
+++ Laboratorinis2Tikras/Laboratorinis2Tikras/Laboratorinis2Tikras/Page2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorinis2Tikras
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Clicked(object sender, EventArgs e)
+        {
+            userInput.Text
+        }
+    }
+}
